4a59e5f42176edce33b6d587b2b5e92ea624f471
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2022-08-28 00:27:28 +0200

    Add more minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), compress()],
   output: 'static',
   site: 'https://sporiff.dev',
   markdown: {
