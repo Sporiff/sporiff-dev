9345ca10d1a9912821277cb7d8b5e6be44eb3919
Sporiff <ciaranainsworth@posteo.net>
2023-04-01 00:23:27 +0200

    Remove mdx from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind(), markdoc()],
+  integrations: [compress(), tailwind(), markdoc()],
   output: "static",
   site: "https://sporiff.dev"
 });
